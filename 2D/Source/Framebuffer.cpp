e6d3720480dd365e3656fd158a410ca096e5412e
HeffleyA <aheffley@student.neumont.edu>
2024-10-06 12:29:34 -0600

    Framebuffer Primitives

--- 2D/Source/Framebuffer.cpp
+++ 2D/Source/Framebuffer.cpp
@@ -43,11 +43,165 @@ void Framebuffer::DrawPoint(int x, int y, const color_t& color)
 
 void Framebuffer::DrawRect(int x, int y, int w, int h, const color_t& color)
 {
-	for (int i = 0; i <= w; i++)
+	if (x >= m_width || x + w < 0 || y + h < 0 || y >= m_height) return;
+
+	int x1 = std::max(x, 0);
+	int x2 = std::min(x + w, m_width);
+	int y1 = std::max(y, 0);
+	int y2 = std::min(y + h, m_height);
+
+	for (int sy = y1; sy < y2; sy++)
 	{
-		for (int i2 = 0; i2 <= h; i2++)
+		int index = x1 + sy * m_width;
+		std::fill(m_buffer.begin() + index, m_buffer.begin() + (index + x2 - x1), color);
+		//for (int sx = x1; sx < x2; sx++)
 		{
-			DrawPoint(x + i, y + i2, color);
+			//m_buffer[sx + sy * m_width] = color;
+			//m_buffer[index++] = color;
 		}
+	}
+}
+
+void Framebuffer::DrawLine(int x1, int y1, int x2, int y2, const color_t& color)
+{
+	// calculate deltas
+
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+
+	// check for steep line
+
+	bool steep = std::abs(dy) > std::abs(dx);
+
+	if (steep)
+	{
+		std::swap(x1, y1);
+		std::swap(x2, y2);
+	}
+
+	// ensure left to right drawing
+
+	if (x1 > x2)
+	{
+		std::swap(x1, x2);
+		std::swap(y1, y2);
+	}
+
+	// recalculate deltas
+
+	dx = x2 - x1;
+	dy = std::abs(y2 - y1);
+
+	// set error term and y step direction
+
+	int error = dx / 2;
+	int ystep = (y1 < y2) ? 1 : -1;
+
+	// draw line points
+	for (int x = x1, y = y1; x <= x2; x++)
+	{
+		steep ? DrawPoint(y, x, color) : DrawPoint(x, y, color);
+		error -= dy;
+
+		// update error term
+
+		if (error < 0)
+		{
+			y += ystep;
+			error += dx;
+		}
+	}
+}
+
+void Framebuffer::DrawLineSlope(int x1, int y1, int x2, int y2, const color_t& color)
+{
+	int dx = x2 - x1; //run
+	int dy = y2 - y1; //rise
+
+	// y = mx + b (slope intercept)
+	// m = rise / run (slope)
+
+	if (dx == 0)
+	{
+		if (y1 > y2) std::swap(y1, y2);
+
+		for (int y = y1; y < y2; y++)
+		{
+			m_buffer[x1 + y * m_width] = color;
+		}
+	}
+	else
+	{
+		float m = dy / (float)dx;
+
+		// b = y - (m * x) (y intercept)
+
+		float b = y1 - (m * x1);
+
+		// draw line points
+
+		if (std::abs(dx) > std::abs(dy))
+		{
+			for (int x = x1; x <= x2; x++)
+			{
+				int y = (int)round((m * x) + b);
+				m_buffer[x + y * m_width] = color;
+			}
+		}
+		else
+		{
+			for (int y = y1; y <= y2; y++)
+			{
+				int x = (int)round((y - b) / m);
+				m_buffer[x + y * m_width] = color;
+			}
+		}
+	}
+}
+
+void Framebuffer::DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, const color_t& color)
+{
+	DrawLine(x1, y1, x2, y2, color);
+	DrawLine(x2, y2, x3, y3, color);
+	DrawLine(x3, y3, x1, y1, color);
+}
+
+void Framebuffer::DrawCircle(int xc, int yc, int radius, const color_t& color)
+{
+	int x = 0;
+	int y = radius;
+	int d = 3 - (2 * radius);
+
+	DrawPoint(xc + x, yc + y, color);
+	DrawPoint(xc - x, yc + y, color);
+	DrawPoint(xc + x, yc - y, color);
+	DrawPoint(xc - x, yc - y, color);
+	DrawPoint(xc + y, yc + x, color);
+	DrawPoint(xc - y, yc + x, color);
+	DrawPoint(xc + y, yc - x, color);
+	DrawPoint(xc - y, yc - x, color);
+
+	while (y >= x)
+	{
+		if (d < 0)
+		{
+			d = d + (4 * x) + 6;
+		}
+		else
+		{
+			y--;
+			d = d + 4 * (x - y) + 10;
+		}
+
+		x++;
+
+		DrawPoint(xc + x, yc + y, color);
+		DrawPoint(xc - x, yc + y, color);
+		DrawPoint(xc + x, yc - y, color);
+		DrawPoint(xc - x, yc - y, color);
+		DrawPoint(xc + y, yc + x, color);
+		DrawPoint(xc - y, yc + x, color);
+		DrawPoint(xc + y, yc - x, color);
+		DrawPoint(xc - y, yc - x, color);
 	}
 }
--- 2D/Source/Main.cpp
+++ 2D/Source/Main.cpp
@@ -30,14 +30,29 @@ int main(int argc, char* argv[])
 		}
 		framebuffer.Clear(color_t{ 0, 0, 0, 255 });
 
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 1; i++)
 		{
-			int x = rand() % 400;
-			int y = rand() % 300;
-			framebuffer.DrawPoint(x, y, { 255, 255, 255, 255 });
+			int x1 = rand() % framebuffer.m_width;
+			int y1 = rand() % framebuffer.m_height;
+			int x2 = rand() % framebuffer.m_width;
+			int y2 = rand() % framebuffer.m_height;
+			int x3 = rand() % framebuffer.m_width;
+			int y3 = rand() % framebuffer.m_height;
+
+			//framebuffer.DrawPoint(x, y, { 255, 255, 255, 255 });
+			//framebuffer.DrawLine(x1, y1, x2, y2, { 255, 255, 255, 255 });
+			//framebuffer.DrawTriangle(x1, y1, x2, y2, x3, y3, { 255, 255, 255, 255 });
+			//framebuffer.DrawCircle(x1, y1, r, { 255, 255, 255, 255 });
 		}
 
-		framebuffer.DrawRect(150, 150, 60, 50, { 255, 255, 0, 255 });
+		framebuffer.DrawCircle(100, 100, 30, { 255, 255, 255, 255 });
+
+		framebuffer.DrawTriangle(100, 200, 50, 200, 50, 100, {255, 255, 255, 255});
+
+		framebuffer.DrawLine(300, 200, 350, 250, { 255, 255, 255, 255 });
+
+		//framebuffer.DrawRect(150, 150, 60, 50, { 255, 255, 0, 255 });
+
 		framebuffer.Update();
 
 		renderer = framebuffer;
